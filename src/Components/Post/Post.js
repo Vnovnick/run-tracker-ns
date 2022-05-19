d695faafb24109c8bcaca56647e2b29b908fbaa4
Vnovnick <safonov.nick10@gmail.com>
2022-05-19 17:28:48 -0500

    Test commit

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,1 +1,14 @@
+import React from 'react'
+
+export default function Post() {
+  return (
+    <div className='post-info'>
+        <h1>Post Title</h1>
+        <p>Post description</p>
+        <h3>distance</h3>
+        <h3>Average Pace</h3>
+        <h3>Moving Time</h3>
+    </div>
+  )
+};
 
--- src/Components/PostFeed/PostFeed.js
+++ src/Components/PostFeed/PostFeed.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import Post from '../Post/Post';
+
+
+export default function PostFeed() {
+  return (
+    <div className='post-feed'>
+        <Post />
+    </div>
+  )
+}
 
