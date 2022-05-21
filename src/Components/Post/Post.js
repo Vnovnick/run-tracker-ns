b0f11f73962a4593430bb39bce156869a16ef5c2
Vnovnick <safonov.nick10@gmail.com>
2022-05-21 18:54:43 -0500

    adding axios

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import axios from '../../util/axios';
+
+let accessToken = "b99d56cc7b18aa3527edff334a58b370119d8bfa";
+
+const clientID = '84711';
+const clientSecret = '2656645c7185a01fbbf85c8bbbdf1d4d24273510';
+const dataUrl = 'api/v3/athlete/activities';
 
 
 export default function Post() {  
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData(){
+      const request = await axios.get(`${dataUrl}?access_token=${accessToken}`);
+      console.log(request.data);
+      setData(request.data);
+    }
+    fetchData();
     
+  }, []);
+
+  const items = {
+    name: data.name,
+
+  };
+  
 
   return (
     <div className='post-info'>
-        <h1>Title</h1>
-        
+        {data.map(item => <h3 key={item.id}>{item.name}</h3>)}
+
+        {/* <p></p>
         <p>Post description</p>
         <h3>distance</h3>
         <h3>Average Pace</h3>
-        <h3>Moving Time</h3>
+        <h3>Moving Time</h3> */}
     </div>
   )
 };
