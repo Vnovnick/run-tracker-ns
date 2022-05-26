3574f19029617e73458447b9a3863b404e9ae2d8
Vnovnick <safonov.nick10@gmail.com>
2022-05-25 19:34:55 -0500

    hide data

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -3,10 +3,7 @@ import axios from '../../util/axios';
 import './Post.css';
 import { stravaApiData } from '../../apiData';
 
-// export const stravaApiData = {
-//   client_id: '84711',
-//   client_secret: '2656645c7185a01fbbf85c8bbbdf1d4d24273510',
-// };
+
 
 
 
