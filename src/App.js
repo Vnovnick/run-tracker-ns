4602a9b562f32c5221ecd205ace5c3d48e000a55
Vnovnick <safonov.nick10@gmail.com>
2022-06-26 11:46:22 -0500

    figured out body background color

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import PostFeed from './Components/PostFeed/PostFeed';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
       </header>     
         <main>
         <PostFeed />
