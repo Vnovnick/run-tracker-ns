5559a3150ea8f7d0ad0f71c0313ce21ccb2fc984
Vnovnick <safonov.nick10@gmail.com>
2022-06-26 10:31:08 -0500

    adding titlescreen.scss

--- src/Components/TitleScreen/TitleScreen.js
+++ src/Components/TitleScreen/TitleScreen.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './TitleScreen.scss';
 
 export default function TitleScreen() {
   return (
-    <div>
+    <div id='title-screen'>
         <h1>Run_Tracker</h1>
         <p>Please log-in to view Strava Data and to access Spotify log-in</p>
     </div>
